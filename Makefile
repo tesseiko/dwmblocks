5e05b5bddea90a1b59a9394dcccd6cc5cc8a6eec
lefmanos <lefmanos@protonmail.com>
2022-03-11 16:54:05 +0200

    init commit
    fork from https://github.com/lukesmithxyz/dwm.git

---
+++ Makefile
@@ -0,0 +1,22 @@
+.POSIX:
+
+PREFIX = /usr/local
+CC = gcc
+
+dwmblocks: dwmblocks.o
+	$(CC) dwmblocks.o -lX11 -o dwmblocks
+dwmblocks.o: dwmblocks.c config.h
+	$(CC) -c dwmblocks.c
+clean:
+	rm -f *.o *.gch dwmblocks
+install: dwmblocks
+	mkdir -p $(DESTDIR)$(PREFIX)/bin
+	cp -f dwmblocks $(DESTDIR)$(PREFIX)/bin
+	chmod 755 $(DESTDIR)$(PREFIX)/bin/dwmblocks
+	$(MAKE) -C statusbar
+
+uninstall:
+	rm -f $(DESTDIR)$(PREFIX)/bin/dwmblocks
+	$(MAKE) clean -C statusbar
+
+.PHONY: clean install uninstall
---
+++ config.h
@@ -0,0 +1,28 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",	"sb-tasks",					10,	26},
+	{"",	"sb-mode",					0,	27},
+	{"",	"sb-clock",					60,	1},
+	{"",	"sb-music",					0,	11},
+	// {"",	"sb-adhd",		0,	6},
+	// {"",	"sb-pacpackages",		0,	8},
+	// {"",	"sb-torrent",		20,	7},
+	{"",	"sb-volume",				0,	10},
+	{"",	"sb-memory",				10,	14},
+	{"",	"sb-cpubars",				10,	18},
+	{"",	"sb-nettraf",				1,	16},
+	/*{"",	"sb-battery",				5,	3},*/
+	{"",	"sb-internet",				5,	4},
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = "";
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
+
